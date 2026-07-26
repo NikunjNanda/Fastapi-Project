ab12ddda9c65dfc1235a80706eb68ea9dcc08b2f
NikunjNanda <nikunjnanda730@gmail.com>
2026-07-26 15:12:27 +0530

    updated the services completely

--- app/services/model_service.py
+++ app/services/model_service.py
@@ -1,7 +1,7 @@
 import joblib 
 import pandas as pd 
 from app.core.config import settings
-from app.cache.redis_cache import set_cache_prediction,get_cached_prediction 
+from app.cache.redis_cache import set_cached_prediction,get_cached_prediction 
 
 model = joblib.load(settings.MODEL_PATH) 
 
@@ -13,6 +13,6 @@ def predict_car_price(data: dict):
 
     input_data = pd.DataFrame([data])
     prediction = model.predict(input_data)[0] 
-    set_cache_prediction(cache_key,prediction)
+    set_cached_prediction(cache_key,prediction) 
     return prediction 
 
