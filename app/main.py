02666c8dda11df6ced4fe4618687450bba4236a4
NikunjNanda <nikunjnanda730@gmail.com>
2026-07-26 15:26:12 +0530

    updated teh route now

--- app/main.py
+++ app/main.py
@@ -1,21 +1,25 @@
-from fastapi import FastAPI 
-from prometheus_fastapi_instrumentator import Instrumentator 
-from app.api import routes_auth , routes_predict 
-from app.middleware.logging_middleware import LoggingMiddleware 
-from app.core.exception import register_exception_handlers 
+from fastapi import FastAPI
+from prometheus_fastapi_instrumentator import Instrumentator
+from app.api import routes_auth, routes_predict
+from app.middleware.logging_middleware import LoggingMiddleware
+from app.core.exception import register_exception_handlers
 
-app = FastAPI(title='Car Price Prediction API') 
+app = FastAPI(title="Car Price Prediction API")
 
-#Linking the middleware to this application 
 app.add_middleware(LoggingMiddleware)
 
-#Linkign the endpoints
-app.include_router(routes_auth.router , tags=['Auth'])
-app.include_router(routes_predict.router , tags=['Prediction']) 
+app.include_router(routes_auth.router, tags=["Auth"])
+app.include_router(routes_predict.router, tags=["Prediction"])
 
-#monitoring using prometheus 
-Instrumentator().instrument(app).expose(app) 
+Instrumentator().instrument(app).expose(app)
 
-#add exception handler
-register_exception_handlers(app) 
+register_exception_handlers(app)
 
+# Home route
+@app.get("/")
+async def home():
+    return {
+        "message": "Car Price Prediction API is running successfully!",
+        "docs": "/docs",
+        "metrics": "/metrics"
+    }
